8379f06398ed97937b4b76252b839402fc229f0d
yangrq <yangrq@kuxun.com>
2013-10-11 19:22:33 +0800

    encoding

--- php/keyword_encoding.php
+++ php/keyword_encoding.php
@@ -1,15 +1,31 @@
 <?php
+
+define('BASE_PATH', dirname(__FILE__));
+$logname = BASE_PATH . '/log/encoding.' . date("Y-m-d") . '.log';
+
+function locallog($s) {
+    global $logname;
+    $ns = date('H-i-s: ') . $s . "\n";
+    file_put_contents($logname, $ns, FILE_APPEND);
+}
+
+
+$_GET['word'] = '中国';
+
 if (!isset($_GET['word'])) {
     echo "usage: path?word=xx";
     exit();
 }
 
 $word = $_GET['word'];
-$encodeing = mb_detect_encoding($word, "UTF-8,GBK,GB2312,CP936,ASCII" );
-if ($encodeing != "UTF-8")
+$encoding = mb_detect_encoding($word, "UTF-8,GBK,GB2312,CP936,ASCII" );
+locallog("$word encoding: $encoding");
+
+if ($encoding != "UTF-8")
 {
-        $word = mb_convert_encoding($word, "UTF-8", $encodeing);
+        $word = mb_convert_encoding($word, "UTF-8", $encoding);
 }
 
 $word_encoded = urlencode($word);
+locallog("$word encoding: $encoding, encoded: $word_encoded");
 echo $word_encoded;
